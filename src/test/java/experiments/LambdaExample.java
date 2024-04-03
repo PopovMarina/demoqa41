c72cacbc94f913eed00a73c52ad24a837b5dbd30
PopovMarina <pilgrim7474@gmail.com>
2024-04-03 19:06:26 +0300

    Webinar_13 - handler

--- src/test/java/experiments/LambdaExample.java
+++ src/test/java/experiments/LambdaExample.java
@@ -1,9 +1,20 @@
 package experiments;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class LambdaExample {
+
+    public static void increasePrice() {
+        List<Double> prices = Arrays.asList(10.0,20.0,30.0);
+        prices.replaceAll(price -> price * 1.1);
+        prices.forEach(System.out::println);
+    }
+
     public static void main(String[] args) {
+        increasePrice();
+
         ArrayList<Integer> arrL = new ArrayList<Integer>();
         arrL.add(1);
         arrL.add(2);
